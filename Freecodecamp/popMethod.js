7eb994806f6932d1e82e8f3ff9add6e4e5d9a578
Matheus Gomes de Moura <Matheus.gomesdemouraa@gmail.com>
2022-08-20 23:58:04 -0300

    pop method

---
+++ Freecodecamp/popMethod.js
@@ -0,0 +1,8 @@
+const myArray = [["John", 23 ], ["cat", 2]];
+const threeArr = [1, 2 ,3]
+const oneDown = threeArr.pop();
+const removedArray = myArray.pop();
+
+console.log("My Array:", myArray);
+console.log(`ThreeArray: ${oneDown}`);
+console.log(`removedArray: ${removedArray}`);
