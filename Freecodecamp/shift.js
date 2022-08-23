78e3b7edd1ee5b49cee9d5d3dcc1ceb269756609
Matheus Gomes de Moura <Matheus.gomesdemouraa@gmail.com>
2022-08-22 23:58:02 -0300

    Array shift method

---
+++ Freecodecamp/shift.js
@@ -0,0 +1,7 @@
+// remove first element in array
+
+const myArray = [["John", 23], ["dog", 3]];
+
+const removedFromMyArray  = myArray.shift();
+
+console.log("removedFromMyArray:", removedFromMyArray, "myArray:", myArray);
