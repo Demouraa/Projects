5427cedac2403d81bbeffa3a7684e6f7cb45bceb
Matheus Gomes de Moura <Matheus.gomesdemouraa@gmail.com>
2022-08-25 23:58:29 -0300

    add function with args

---
+++ Freecodecamp/7.funcWithArgs.js
@@ -0,0 +1,10 @@
+function functionWithArgs(a,b) {
+    let c = a + b
+    return c
+  }
+  
+  let a = functionWithArgs(1,2);
+  let b = functionWithArgs(7,9);
+  
+  console.log(a);
+  console.log(b);
