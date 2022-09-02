64a7edbe5312ca25e31c36bcfa4a926e359bd4b7
Matheus Gomes de Moura <Matheus.gomesdemouraa@gmail.com>
2022-09-02 00:22:25 -0300

    add componente product-alerts

--- Angular/start/src/app/product-list/product-list.component.ts
+++ Angular/start/src/app/product-list/product-list.component.ts
@@ -4,7 +4,7 @@ import { products } from '../products';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
+  templateUrl: './product-list.component.html', 
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
@@ -16,7 +16,11 @@ export class ProductListComponent implements OnInit {
   }
 
   share() {
-    window.alert("The product has been shared");
+    window.alert("The product has been shared!");
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
   }
 
 }
