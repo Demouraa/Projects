d2dd59786de12e451ce96c3b0fb29be66f2ceded
Matheus Gomes de Moura <Matheus.gomesdemouraa@gmail.com>
2022-09-29 00:00:21 -0300

    .

--- Angular/start/src/app/app-routing.module.ts
+++ Angular/start/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { CheckoutComponent } from './checkout/checkout.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
