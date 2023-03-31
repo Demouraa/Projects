55ce8d5f2ce38d104051d013b95d2dc925ee7be1
Matheus Gomes de Moura <Matheus.gomesdemouraa@gmail.com>
2023-03-31 11:11:39 -0300

    exercicios freecodeCamp

---
+++ Freecodecamp/basic-javascript/1-build-javascript-objects.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    "name": "",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Matheus", "Leonardo"]
+  
+    // Only change code above this line
+  };
